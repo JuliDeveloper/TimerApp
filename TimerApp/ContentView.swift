7aeeac28b0a411d4bda0312e963549d4e386b587
JuliDeveloper <rja-97@yandex.ru>
2022-09-12 19:53:00 +0300

    Реализовала передачу имени пользователя на экран приветствия и счетчика

--- TimerApp/ContentView.swift
+++ TimerApp/ContentView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject private var timer = TimeCounter()
+    @EnvironmentObject private var userManager: UserManager
         
     var body: some View {
         VStack {
+            Text("Hi, \(userManager.name)")
+                .padding(.top, 100)
+                .font(.largeTitle)
             Text("\(timer.counter)")
                 .padding(.top, 100)
                 .font(.largeTitle)
@@ -26,5 +30,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(UserManager())
     }
 }
