76707e2736c37e035a3aa21547a1480db466e64f
JuliDeveloper <rja-97@yandex.ru>
2022-09-12 20:30:24 +0300

    Добавила счетчик символов, который проверяет длину строки и показывает число + проверка валидности строки

--- TimerApp/LoginView.swift
+++ TimerApp/LoginView.swift
@@ -10,23 +10,34 @@ import SwiftUI
 struct LoginView: View {
     
     @State var userName: String = ""
+    @State var countWords: Int = 0
+    
     @EnvironmentObject private var user: UserManager
     
     var body: some View {
         VStack(spacing: 20) {
-            TextField("Enter your name...", text: $userName)
-                .multilineTextAlignment(.center)
+            HStack {
+                TextField("Enter your name...", text: $userName)
+                    .multilineTextAlignment(.center)
+                Text("\(countWords)")
+                    .foregroundColor(countWords < 3 ? .red : .green)
+                    .onChange(of: userName.count) { newValue in
+                        countWords = newValue
+                    }
+            }
+            .padding()
             Button(action: loginUser) {
                 HStack {
                     Image(systemName: "checkmark.circle")
                     Text("OK")
                 }
             }
+            .disabled(countWords < 3 ? true : false)
         }
     }
     
     private func loginUser() {
-        if !userName.isEmpty {
+        if !userName.isEmpty && userName.count > 2 {
             user.name = userName
             user.isLogin.toggle()
         }
