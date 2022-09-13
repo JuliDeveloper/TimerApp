807e01ca3c5b87965a31a26212ead2e0c0b99c7f
JuliDeveloper <rja-97@yandex.ru>
2022-09-13 17:49:03 +0300

    Добавила структуру User и изменила класс DataManager

--- TimerApp/UserManager.swift
+++ TimerApp/UserManager.swift
@@ -8,6 +8,21 @@
 import Combine
 
 final class UserManager: ObservableObject {
-    @Published var isLogin = false
+    @Published var user = User()
+    
+    var nameIsValid: Bool {
+        user.name.count >= 3
+    }
+    
+    init() {}
+    
+    init(user: User) {
+        self.user = user
+    }
+    
+}
+
+struct User: Codable {
     var name = ""
+    var isLogin = false
 }
